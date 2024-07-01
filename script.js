7867e57e6fca9acf787bff76b3f0e3041115e32b
Suraj Pawar <spkings1263@gmail.com>
2024-07-01 22:14:45 +0530

    Revert "Calculator Program"

--- script.js
+++
@@ -1,9 +0,0 @@
-// Clears the screen on click of C button.
-function clearScreen() {
-    document.getElementById("result").value = "";
-  }
-  // Displays entered value on screen.
-  function liveScreen(value) {
-    document.getElementById("result").value += value;
-  }
- 
